2a6fa6c75195b44fe65368aa39b582682f1bb6cf
Muhammad Safaat <paatlupi01@gmail.com>
2023-09-11 16:38:53 +0700

    update

--- src/features/navbar/NavigationBar.tsx
+++ src/features/navbar/NavigationBar.tsx
@@ -22,70 +22,72 @@ export const NavigationBar = () => {
 
     return (
         <>
-            <Navbar
-                className={`
+            <div className="shadow-sm">
+                <Navbar
+                    className={`
                     ${!scrolled ? "" : styles["nav-fixed"]} 
-                    ${styles.container} ${styles.nav} shadow-sm`
-                }
-            >
-                <Stack className="flex-row position-relative me-3 me-md-5" >
-                    {/* Search Catatan dimensi Tablet and Mobile */}
-                    <Stack
-                        data-testid="input-search-mobile"
-                        className={`${styles["parent-search-mobile"]} ${!activeInputSearch ? styles["search-notActive"] : styles["search-active"]}`}
-                    >
-                        <SearchCatatan tipe={"mobile"} />
-                    </Stack>
-
-                    {/* Button Sidebar */}
-                    <Button className={`${styles["btn-menu-utama-nav"]} align-self-center me-3`}>
-                        <HiOutlineBars4 />
-                    </Button>
-
-                    {/* Icon Name Website */}
-                    <Navbar.Brand
-                        role="heading"
-                        className={`${styles["judul-nav"]} align-self-center`}
-                    >
-                        Keep Aduh
-                    </Navbar.Brand>
-
-                    {/* Parent Search Dimensi Pc */}
-                    <Stack className={styles["parent-search"]}>
-                        {/* Search Dimensi PC */}
-                        <Stack className={`${styles["search-catatan"]} justify-content-center align-self-end`}>
-                            <SearchCatatan tipe={"noMobile"} />
+                    ${styles["global-container"]} ${styles.nav}`
+                    }
+                >
+                    <Stack className="flex-row position-relative me-3 me-md-5" >
+                        {/* Search Catatan dimensi Tablet and Mobile */}
+                        <Stack
+                            data-testid="input-search-mobile"
+                            className={`${styles["parent-search-mobile"]} ${!activeInputSearch ? styles["search-notActive"] : styles["search-active"]}`}
+                        >
+                            <SearchCatatan tipe={"mobile"} />
                         </Stack>
 
-                        {/* Button Active Search Dimensi Tablet And Mobile */}
-                        <Button
-                            className={`${styles["btn-search-on-off"]}`}
-                            onClick={() => dispatch(activeSearch(!activeInputSearch))}
-                        >
-                            <BsSearch />
+                        {/* Button Sidebar */}
+                        <Button className={`${styles["btn-menu-utama-nav"]} align-self-center me-3`}>
+                            <HiOutlineBars4 />
                         </Button>
-                    </Stack>
-                </Stack>
 
-                {/* Button Login */}
-                {dataLoginUsers?.uuid === undefined
-                    ? (
-                        <Buttons
-                            styleScss={"btn"}
-                            stylesBtn={{ width: "6rem", height: "2.5rem", fontSize: "1.2rem" }}
-                            onClicks={() => {
-                                toggle(true)
-                                dispatch(activeFormTransition({ onOffForm: true }))
-                            }}
+                        {/* Icon Name Website */}
+                        <Navbar.Brand
+                            role="heading"
+                            className={`${styles["judul-nav"]} align-self-center`}
                         >
-                            Login
-                        </Buttons>
-                    )
-                    : (
-                        <UsersLogin />
-                    )
-                }
-            </Navbar >
+                            Keep Aduh
+                        </Navbar.Brand>
+
+                        {/* Parent Search Dimensi Pc */}
+                        <Stack className={styles["parent-search"]}>
+                            {/* Search Dimensi PC */}
+                            <Stack className={`${styles["search-catatan"]} justify-content-center align-self-end`}>
+                                <SearchCatatan tipe={"noMobile"} />
+                            </Stack>
+
+                            {/* Button Active Search Dimensi Tablet And Mobile */}
+                            <Button
+                                className={`${styles["btn-search-on-off"]}`}
+                                onClick={() => dispatch(activeSearch(!activeInputSearch))}
+                            >
+                                <BsSearch />
+                            </Button>
+                        </Stack>
+                    </Stack>
+
+                    {/* Button Login */}
+                    {dataLoginUsers?.uuid === undefined
+                        ? (
+                            <Buttons
+                                styleScss={"btn"}
+                                stylesBtn={{ width: "6rem", height: "2.5rem", fontSize: "1.2rem" }}
+                                onClicks={() => {
+                                    toggle(true)
+                                    dispatch(activeFormTransition({ onOffForm: true }))
+                                }}
+                            >
+                                Login
+                            </Buttons>
+                        )
+                        : (
+                            <UsersLogin />
+                        )
+                    }
+                </Navbar >
+            </div>
         </>
     )
 }
