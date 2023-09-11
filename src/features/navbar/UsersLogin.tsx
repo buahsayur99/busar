db30e427bf7eebc4e9623724c1043aa7b904406f
Muhammad Safaat <paatlupi01@gmail.com>
2023-09-11 16:25:53 +0700

    update navBar

--- src/features/navbar/UsersLogin.tsx
+++ src/features/navbar/UsersLogin.tsx
@@ -1,14 +1,52 @@
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import styles from "../../style/index.module.scss";
 import { FaUserAlt } from "../../utils/icons";
+import { MobileUsers } from "./MobileUsers";
+import { resetDataLoginUsers } from "../../app/actions/apiUsersSlice";
 
 export const UsersLogin = () => {
+    const dispatch = useAppDispatch()
+    const { dataLoginUsers } = useAppSelector(state => state.apiUsers);
+
     return (
         <>
-            <div>
+            <div className={`${styles["parent-users-login"]}`}>
                 <span className={`${styles["parent-icon-users"]}`}>
                     <FaUserAlt />
                 </span>
+
+                <div className={`${styles["parent-users-hover"]}`}>
+                    <div className={`${styles["data-users"]}`}>
+                        <div className={`${styles["users-profile"]}`}>
+                            <FaUserAlt />
+                        </div>
+                        <p>{dataLoginUsers?.email}</p>
+                    </div>
+
+                    <div className={`${styles["parent-users-menu"]}`}>
+                        <ul>
+                            <li>
+                                <button
+                                    type="button"
+                                >
+                                    Settings
+                                </button>
+                            </li>
+                            <hr />
+                            <li>
+                                <button
+                                    type="button"
+                                    onClick={() => dispatch(resetDataLoginUsers())}
+                                >
+                                    Sign out
+                                </button>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
+
+            <MobileUsers />
         </>
     )
 }
