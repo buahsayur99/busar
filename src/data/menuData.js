9b5c9cf23697ddd046cd0112f0224ce5c3934729
Muhammad Safaat <paatlupi01@gmail.com>
2023-09-18 08:10:42 +0700

    add folder data

---
+++ src/data/menuData.js
@@ -0,0 +1,8 @@
+// profile setting menu data
+const menuData = [
+    { nameLink: "general", toLink: "/account" },
+    { nameLink: "edit profile", toLink: "/account/profile" },
+    { nameLink: "password", toLink: "/account/password" }
+];
+
+export default menuData;
