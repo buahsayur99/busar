1af1ec33c43fb96bb1baa422362c26ef51da2d68
Muhammad Safaat <paatlupi01@gmail.com>
2023-09-22 10:16:46 +0700

    add useAuthUsers and useSaveLastPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,36 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Home } from "./pages/Home";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { PrivateRoutes } from "./routes/PrivateRoutes";
 import { General, Profile, Password } from "./pages/pengaturanProfile/index";
+import { useSaveLastPage } from "./hook/useSaveLastPage";
+import { useAuthUsers } from "./hook/useAuthUsers";
 
 function App() {
+  useAuthUsers();
+  const { infoHalaman } = useSaveLastPage();
+
   return (
     <>
-      <div style={{ height: "100rem" }}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="account" element={<PrivateRoutes />}>
-            <Route index element={<General />} />
-            <Route path="password" element={<Password />} />
-            <Route path=":section" element={<Profile />} />
-          </Route>
-        </Routes>
-      </div>
+      {!infoHalaman.is_authenticated ? (
+        <h1>Loading</h1>
+      ) : (
+        <Router>
+          <div style={{ height: "100rem" }}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route
+                path="account"
+                element={<PrivateRoutes />}
+              >
+                <Route index element={<General />} />
+                <Route path="password" element={<Password />} />
+                <Route path=":section" element={<Profile />} />
+              </Route>
+            </Routes>
+          </div>
+        </Router>
+      )}
     </>
   );
 }
