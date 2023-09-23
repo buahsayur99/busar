e55923a94f525a62d3616862caa4b50d7c52b564
Muhammad Safaat <paatlupi01@gmail.com>
2023-09-24 03:50:50 +0700

    add function removeLocalStorage

--- src/hook/useAuthUsers.tsx
+++ src/hook/useAuthUsers.tsx
@@ -1,21 +1,30 @@
 import { useCallback, useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../app/hooks";
 import { authLogin } from "../app/actions/apiUsersSlice";
+import { rejectedAuthLogin } from "../utils/responseApi";
 
 export const useAuthUsers = () => {
-    const { dataLoginUsers } = useAppSelector(state => state.apiUsers);
+    const { dataLoginUsers, isMessage } = useAppSelector(state => state.apiUsers);
     const dispatch = useAppDispatch();
     const uuid = localStorage.getItem("uuid");
 
     const requestUserApi = useCallback(() => {
         const link = `${process.env.REACT_APP_API_URL_LOCAL}/me/${uuid}`;
         dispatch(authLogin({ link }));
-    }, [dispatch, uuid])
+    }, [dispatch, uuid]);
+
+    const removeLocalStorage = useCallback((event: string) => {
+        if (isMessage === rejectedAuthLogin.toLowerCase()) {
+            localStorage.removeItem(event);
+        }
+    }, [isMessage])
 
     useEffect(() => {
         // if dataLoginUsers has uuid, enter uuid in localStorage
         if (dataLoginUsers?.uuid) return localStorage.setItem("uuid", dataLoginUsers?.uuid);
         // if uuid in localstorage exists, run requestUserApi function
         requestUserApi();
-    }, [uuid, dataLoginUsers?.uuid, requestUserApi])
+        removeLocalStorage("uuid");
+        // if (isMessage === rejectedAuthLogin.toLowerCase()) return removeLocalStorage("uuid");
+    }, [uuid, dataLoginUsers?.uuid, requestUserApi, removeLocalStorage])
 }
