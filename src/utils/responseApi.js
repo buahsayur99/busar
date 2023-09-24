0cb5711ce0b2d694354e015192d039b969147595
Muhammad Safaat <paatlupi01@gmail.com>
2023-09-24 13:11:17 +0700

    add response update password sama

--- src/utils/responseApi.js
+++ src/utils/responseApi.js
@@ -1,4 +1,5 @@
 export const matchPass = "Password dan confirmasi password tidak sama";
 export const updateSuccess = "Update user success";
 export const rejectedAuthLogin = "User tidak di temukan";
+export const rejectedUpdatePassword = "Password tetep sama";
 
