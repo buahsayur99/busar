a4d6567d1fef40aba246b771eb30dae700c1523b
Muhammad Safaat <paatlupi01@gmail.com>
2023-09-24 11:47:43 +0700

    add response Api's

---
+++ src/utils/responseApi.js
@@ -0,0 +1,4 @@
+export const matchPass = "Password dan confirmasi password tidak sama";
+export const updateSuccess = "Update user success";
+export const rejectedAuthLogin = "User tidak di temukan";
+
