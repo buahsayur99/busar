75e00c4c84ddad8b0c101fe3db65a33971aae0b5
Muhammad Safaat <paatlupi01@gmail.com>
2023-09-24 16:33:14 +0700

    add router Address

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Home } from "./pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { PrivateRoutes } from "./routes/PrivateRoutes";
-import { General, Profile, Password } from "./pages/pengaturanProfile/index";
+import { General, Profile, Password, Address } from "./pages/pengaturanProfile/index";
 import { useSaveLastPage } from "./hook/useSaveLastPage";
 import { useAuthUsers } from "./hook/useAuthUsers";
 
@@ -12,9 +12,7 @@ function App() {
 
   return (
     <>
-      {!infoHalaman.is_authenticated ? (
-        <h1>Loading</h1>
-      ) : (
+      {infoHalaman.is_authenticated && (
         <Router>
           <div style={{ height: "100rem" }}>
             <Routes>
@@ -26,6 +24,7 @@ function App() {
                 <Route index element={<General />} />
                 <Route path="password" element={<Password />} />
                 <Route path=":section" element={<Profile />} />
+                <Route path="address" element={<Address />} />
               </Route>
             </Routes>
           </div>
