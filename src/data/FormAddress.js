666855c7b1730f93610e49e9357eb18917850c51
Muhammad Safaat <paatlupi01@gmail.com>
2023-09-24 16:34:19 +0700

    add file data form address

---
+++ src/data/FormAddress.js
@@ -0,0 +1,19 @@
+export const dataInputFormAddress = {
+    name: "",
+    numberPhone: "",
+    city: "",
+    subdistrict: "",
+    codePos: "",
+    completeAddress: "",
+    courierNote: ""
+}
+
+export const dataValidasiInputFormAddress = {
+    name: { status: false, text: "" },
+    numberPhone: { status: false, text: "" },
+    city: { status: false, text: "" },
+    subdistrict: { status: false, text: "" },
+    codePos: { status: false, text: "" },
+    completeAddress: { status: false, text: "" },
+    courierNote: { status: false, text: "" }
+}
