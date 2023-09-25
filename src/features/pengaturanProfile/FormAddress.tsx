6c347a2d43658d90ad335d7cb53b6959b83e200b
Muhammad Safaat <paatlupi01@gmail.com>
2023-09-26 00:27:19 +0700

    add maxInput and cssMaxInput pada component InputsForm

--- src/features/pengaturanProfile/FormAddress.tsx
+++ src/features/pengaturanProfile/FormAddress.tsx
@@ -1,43 +1,127 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "../../style/index.module.scss";
 import { InputsForm } from "../../components/InputsForm";
-import { dataInputFormAddress, dataValidasiInputFormAddress } from "../../data/FormAddress";
+import { IoIosCloseCircle } from "../../utils/icons";
+import { useInputFormAddress } from "../../hook/useInputFormAddress";
 
 export const FormAddress = () => {
-    // State
-    const [input, setInput] = useState(dataInputFormAddress);
-    const [validasiInput, setValidasiInput] = useState(dataValidasiInputFormAddress);
+    const { input, validasiInput, changeInputValue, changeInputClicked } = useInputFormAddress();
 
-    const resetInput = () => {
-        setInput(dataInputFormAddress);
-    }
-
-    const resetValidasiInput = () => {
-        setValidasiInput(dataValidasiInputFormAddress);
-    }
-
-    const changeInputValue = (event: any) => {
-        setInput(prev => {
-            return { ...prev, ...event }
-        })
-    }
     return (
         <>
             <div className={styles["container-form-address"]}>
                 <div className={styles["bg-white"]}>
-                    {/* Name */}
-                    <InputsForm
-                        cssInput="input-form"
-                        cssPlaceholder="input-placeholder"
-                        cssIcon="input-icon"
-                        cssValidasi="validasi"
-                        typeInput="password"
-                        changeInput={(input) => changeInputValue({ name: input })}
-                        valueInput={input.name}
-                        valuePlaceholder={"Password"}
-                        iconType="PiLockKeyFill"
-                        validasiInput={validasiInput.name}
-                    />
+                    {/* Judul */}
+                    <div className={styles["judul-form"]}>
+                        <h2>add address</h2>
+                        <button
+                            type="button"
+                        >
+                            <IoIosCloseCircle />
+                        </button>
+                        <hr />
+                    </div>
+                    {/* Form */}
+                    <form
+                        className={styles["form-address"]}
+                    >
+                        {/* Name */}
+                        <InputsForm
+                            cssInput="input-form"
+                            cssPlaceholder="input-placeholder"
+                            cssValidasi="validasi"
+                            cssMaxInput="max-input"
+                            typeInput="text"
+                            changeInput={(input) => changeInputValue({ name: input })}
+                            onClicks={() => changeInputClicked({ name: true })}
+                            maxInput={20}
+                            valueInput={input.name}
+                            valuePlaceholder={"Name"}
+                            validasiInput={validasiInput.name}
+                        />
+                        {/* Number Phone */}
+                        <InputsForm
+                            cssInput="input-form"
+                            cssPlaceholder="input-placeholder"
+                            cssValidasi="validasi"
+                            cssMaxInput="max-input"
+                            typeInput="number"
+                            changeInput={(input) => changeInputValue({ numberPhone: input })}
+                            onClicks={() => changeInputClicked({ numberPhone: true })}
+                            maxInput={13}
+                            valueInput={input.numberPhone}
+                            valuePlaceholder={"Number Phone"}
+                            validasiInput={validasiInput.numberPhone}
+                        />
+                        {/* City */}
+                        <InputsForm
+                            cssInput="input-form"
+                            cssPlaceholder="input-placeholder"
+                            cssValidasi="validasi"
+                            cssMaxInput="max-input"
+                            typeInput="text"
+                            changeInput={(input) => changeInputValue({ city: input })}
+                            onClicks={() => changeInputClicked({ city: true })}
+                            maxInput={25}
+                            valueInput={input.city}
+                            valuePlaceholder={"City"}
+                            validasiInput={validasiInput.city}
+                        />
+                        {/* Subdistrict */}
+                        <InputsForm
+                            cssInput="input-form"
+                            cssPlaceholder="input-placeholder"
+                            cssValidasi="validasi"
+                            cssMaxInput="max-input"
+                            typeInput="text"
+                            changeInput={(input) => changeInputValue({ subdistrict: input })}
+                            onClicks={() => changeInputClicked({ subdistrict: true })}
+                            maxInput={30}
+                            valueInput={input.subdistrict}
+                            valuePlaceholder={"Subdistrict"}
+                            validasiInput={validasiInput.subdistrict}
+                        />
+                        {/* CodePos */}
+                        <InputsForm
+                            cssInput="input-form"
+                            cssPlaceholder="input-placeholder"
+                            cssValidasi="validasi"
+                            cssMaxInput="max-input"
+                            typeInput="number"
+                            changeInput={(input) => changeInputValue({ codePos: input })}
+                            onClicks={() => changeInputClicked({ codePos: true })}
+                            maxInput={5}
+                            valueInput={input.codePos}
+                            valuePlaceholder={"CodePos"}
+                            validasiInput={validasiInput.codePos}
+                        />
+                        {/* Complete Address */}
+                        <InputsForm
+                            cssInput="input-form"
+                            cssPlaceholder="input-placeholder"
+                            cssValidasi="validasi"
+                            cssMaxInput="max-input"
+                            typeInput="text"
+                            changeInput={(input) => changeInputValue({ completeAddress: input })}
+                            onClicks={() => changeInputClicked({ completeAddress: true })}
+                            maxInput={200}
+                            valueInput={input.completeAddress}
+                            valuePlaceholder={"Complete Address"}
+                            validasiInput={validasiInput.completeAddress}
+                        />
+                        {/* Courier Note */}
+                        <InputsForm
+                            cssInput="input-form"
+                            cssPlaceholder="input-placeholder"
+                            typeInput="text"
+                            cssMaxInput="max-input"
+                            changeInput={(input) => changeInputValue({ courierNote: input })}
+                            onClicks={() => changeInputClicked({ courierNote: true })}
+                            maxInput={50}
+                            valueInput={input.courierNote}
+                            valuePlaceholder={"Courier Note"}
+                        />
+                    </form>
                 </div>
             </div>
         </>
