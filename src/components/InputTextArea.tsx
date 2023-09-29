9da9a41de95d1f92bc7dbfff9003ae698333e12b
Muhammad Safaat <paatlupi01@gmail.com>
2023-09-29 08:12:52 +0700

    add component inputTextArea

---
+++ src/components/InputTextArea.tsx
@@ -0,0 +1,100 @@
+import React, { useEffect, useRef, useState } from "react";
+import styles from "../style/index.module.scss";
+
+type InputTextAreaProps = {
+    cssInput?: string;
+    cssPlaceholder?: string;
+    cssValidasi?: string;
+    cssMaxInput?: string;
+    maxInput?: number;
+    heights: number;
+    changeInput: (value: string) => void;
+    valueInput: string;
+    valuePlaceholder: string;
+    validasiInput?: {
+        status: boolean | null;
+        text: string;
+    }
+};
+
+export const InputTextArea = ({
+    cssInput, cssPlaceholder, cssValidasi, cssMaxInput,
+    maxInput, changeInput, valueInput, valuePlaceholder, validasiInput, heights
+}: InputTextAreaProps) => {
+    // State
+    const [height, setHeight] = useState(heights);
+    // useRef
+    const textAreaRef = useRef<HTMLTextAreaElement>(null);
+
+    useEffect(() => {
+        const textArea = textAreaRef.current;
+
+        const handleKeyUp = () => {
+            if (textArea) {
+                const scHeight = textArea.scrollHeight;
+                if (scHeight > heights) return setHeight(scHeight);
+            }
+        };
+
+        if (textArea !== null) {
+            textArea.addEventListener("keydown", handleKeyUp);
+        }
+    }, [heights]);
+
+    const handleOnChange = (event: any) => {
+        if (maxInput) {
+            if (event.length <= maxInput) return changeInput(event)
+        }
+    }
+
+    const handleFocusInput = () => {
+        textAreaRef.current?.focus();
+    }
+
+    return (
+        <>
+            <div
+                className={`${styles[`parent-${cssInput}`]}`}
+                onClick={handleFocusInput}
+            >
+                {/* Input */}
+                <textarea
+                    value={valueInput}
+                    onChange={(input) => handleOnChange(input.target.value)}
+                    ref={textAreaRef}
+                    className={`
+                        ${styles[`${cssInput}`]}
+                        ${height > heights && styles[`${cssInput}-padding`]}
+                        ${validasiInput?.status && styles[`${cssInput}-danger`]}
+                    `}
+                    style={{ height: `${height}px` }}
+                />
+                {/* Placeholder */}
+                <p
+                    className={`
+                        ${styles[`${cssPlaceholder}`]} 
+                        ${valueInput?.length > 0 && styles[`${cssPlaceholder}-active`]}
+                        ${validasiInput?.status && styles[`${cssPlaceholder}-danger-focus`]}
+                        ${validasiInput?.status && valueInput.length !== 0 && styles[`${cssPlaceholder}-danger`]}
+                    `}
+                >
+                    {valuePlaceholder}
+                </p>
+                {/* Text Validasi */}
+                <span
+                    className={`${styles[`${cssValidasi}`]}`}
+                >
+                    {validasiInput?.text}
+                </span>
+                {/* Max Input */}
+                {maxInput && (
+                    <div className={styles[`${cssMaxInput}`]}>
+                        <p>{valueInput === null ? "0" : valueInput?.length}</p>
+                        <span>/</span>
+                        <p>{maxInput}</p>
+                    </div>
+                )}
+            </div >
+        </>
+    )
+}
