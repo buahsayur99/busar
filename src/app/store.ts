bb00df9305b668561d269630a93d330a3b2315e4
Muhammad Safaat <paatlupi01@gmail.com>
2023-09-29 08:17:50 +0700

    add  apiAddress: apiAddressSlice

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,12 +2,14 @@ import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import searchCatatanSlice from './actions/searchCatatanSlice';
 import formLoginRegisterSlice from './actions/formLoginRegisterSlice';
 import apiUsersSlice from './actions/apiUsersSlice';
+import apiAddressSlice from "./actions/apiAddressSlice";
 
 export const store = configureStore({
   reducer: {
     searchCatatanSlice: searchCatatanSlice,
     formLoginRegisterSlice: formLoginRegisterSlice,
-    apiUsers: apiUsersSlice
+    apiUsers: apiUsersSlice,
+    apiAddress: apiAddressSlice,
   },
 });
 
