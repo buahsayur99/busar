ecf45aba23cde040bd6adb484b4a0c60d672501c
Muhammad Safaat <paatlupi01@gmail.com>
2023-10-01 09:50:27 +0700

    add icon MdDeselect, MdSelectAll, ImCheckboxChecked

--- src/utils/icons.js
+++ src/utils/icons.js
@@ -3,5 +3,8 @@ import { PiLockKeyFill } from "react-icons/pi" // Closed Lock
 import { PiLockKeyOpenFill } from "react-icons/pi" // Open Lock
 import { IoIosCloseCircle } from "react-icons/io" // Close X
 import { RiLockPasswordFill } from "react-icons/ri" // Lock FormForgetPassword
+import { BsCheck2All } from "react-icons/bs" // Check list 2
+import { MdDeselect, MdSelectAll } from "react-icons/md" // disabled select, select all
+import { ImCheckboxChecked } from "react-icons/im" // CheckBox
 
-export { FaUserAlt, PiLockKeyFill, PiLockKeyOpenFill, IoIosCloseCircle, RiLockPasswordFill };
+export { FaUserAlt, PiLockKeyFill, PiLockKeyOpenFill, IoIosCloseCircle, RiLockPasswordFill, BsCheck2All, MdDeselect, MdSelectAll, ImCheckboxChecked };
