b7aa548fb3a26051ab775364ec354ee88984f48c
Muhammad Safaat <paatlupi01@gmail.com>
2023-10-04 10:30:02 +0700

    add registerSuccess

--- src/utils/responseApi.js
+++ src/utils/responseApi.js
@@ -2,4 +2,5 @@ export const matchPass = "Password dan confirmasi password tidak sama";
 export const updateSuccess = "Update user success";
 export const rejectedAuthLogin = "User tidak di temukan";
 export const rejectedUpdatePassword = "Password tetep sama";
+export const registerSuccess = "Register success";
 
