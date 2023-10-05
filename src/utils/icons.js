3cd7fb689cfad3725db773729299b00e3514d5a5
Muhammad Safaat <paatlupi01@gmail.com>
2023-10-05 12:58:16 +0700

    add ImCheckboxChecked

--- src/utils/icons.js
+++ src/utils/icons.js
@@ -1,10 +1,10 @@
 import { FaUserAlt } from "react-icons/fa"; // Icon User
 import { PiLockKeyFill } from "react-icons/pi" // Closed Lock
 import { PiLockKeyOpenFill } from "react-icons/pi" // Open Lock
-import { IoIosCloseCircle } from "react-icons/io" // Close X
+import { IoIosCloseCircle, IoIosArrowDown } from "react-icons/io" // Close X
 import { RiLockPasswordFill } from "react-icons/ri" // Lock FormForgetPassword
-import { BsCheck2All } from "react-icons/bs" // Check list 2
+import { BsCheck2All, BsTrashFill, BsCheckAll } from "react-icons/bs" // Check list 2, Tempat sampah, Check List 2 tebal
 import { MdDeselect, MdSelectAll } from "react-icons/md" // disabled select, select all
 import { ImCheckboxChecked } from "react-icons/im" // CheckBox
 
-export { FaUserAlt, PiLockKeyFill, PiLockKeyOpenFill, IoIosCloseCircle, RiLockPasswordFill, BsCheck2All, MdDeselect, MdSelectAll, ImCheckboxChecked };
+export { FaUserAlt, PiLockKeyFill, PiLockKeyOpenFill, IoIosCloseCircle, RiLockPasswordFill, BsCheck2All, MdDeselect, MdSelectAll, ImCheckboxChecked, BsTrashFill, IoIosArrowDown, BsCheckAll };
