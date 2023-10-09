95cc33c935b33cb4339f4d7682186de6a9c0fee6
Muhammad Safaat <paatlupi01@gmail.com>
2023-10-10 04:56:48 +0700

    create test useOutsideClick

---
+++ src/hook/__test__/useOutsideClick.test.tsx
@@ -0,0 +1,28 @@
+import { renderHook, act } from "@testing-library/react";
+import { useOutsideClick } from "../useOutsideClick";
+
+describe('useOutsideClick', () => {
+    let ref: React.RefObject<HTMLDivElement>;
+    let faCloseMock: jest.Mock;
+
+    beforeEach(() => {
+        ref = { current: document.createElement('div') };
+        faCloseMock = jest.fn();
+    });
+
+    it('should call faClose when event target is the ref', () => {
+        renderHook(() => useOutsideClick({ ref, faClose: faCloseMock }));
+
+        // Create a mock event with the correct target (your ref)
+        const event = new MouseEvent('click', { bubbles: true });
+        Object.defineProperty(event, 'target', { value: ref.current });
+
+        // Simulate the click event with the correct target
+        act(() => {
+            document.dispatchEvent(event);
+        });
+
+        // Since the event target is the ref, faCloseMock should be called once
+        expect(faCloseMock).toHaveBeenCalledTimes(1);
+    });
+});
