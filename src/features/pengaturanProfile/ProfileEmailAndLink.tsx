73dafafe434f4c8c565c46792d5ffaf83125ede2
Muhammad Safaat <paatlupi01@gmail.com>
2023-10-17 22:18:09 +0700

    fixing profile email and link. and add custome hook useSaveLastPage

--- src/features/pengaturanProfile/ProfileEmailAndLink.tsx
+++ src/features/pengaturanProfile/ProfileEmailAndLink.tsx
@@ -1,37 +1,18 @@
-import React from "react";
-import menuData from "../../data/menuData";
 import { useAppSelector } from "../../app/hooks";
 import styles from "../../style/index.module.scss";
 import { FaUserAlt } from "../../utils/icons";
 import { NavLink } from "react-router-dom";
 import { useSaveLastPage } from "../../hook/useSaveLastPage";
+import { covertInfoHalamanToNameButton } from "../../utils/convert";
 
 export const ProfileEmailAndLink = () => {
     // useAppSelector
     const { dataLoginUsers } = useAppSelector(state => state.apiUsers);
     // Custome Hook
-    useSaveLastPage();
-
-    const convertLinkToName = () => {
-        // Take website liks without domain
-        let currentPageString = localStorage.getItem("informasi_halaman");
-        let page_path;
-        if (currentPageString) page_path = JSON.parse(currentPageString);
-
-        let currentPath = page_path.page_path;
-        // Convert currentPath to string
-        let hasil = currentPath.toString();
-
-        // looping data menu
-        menuData.forEach((data) => {
-            if (data.toLink === hasil) return hasil = data.nameLink
-        })
-
-        return hasil
-    }
+    const { infoHalaman } = useSaveLastPage();
 
     const convertNameToInfo = () => {
-        let link = convertLinkToName();
+        let link = covertInfoHalamanToNameButton(infoHalaman?.page_path)
 
         if (link === "general") return "Update your email and manage your account";
         if (link === "edit profile") return "Add your profile image";
@@ -52,11 +33,13 @@ export const ProfileEmailAndLink = () => {
                     <div className={`${styles["name-profile-email"]}`}>
                         <h2>{dataLoginUsers?.email}</h2>
                         <span>/</span>
-                        <h3 className={`${styles["url-link"]}`}>{convertLinkToName()}
+                        <h3 className={`${styles["url-link"]}`}>
+                            {covertInfoHalamanToNameButton(infoHalaman?.page_path)}
                         </h3>
                     </div>
-
-                    <p>{convertNameToInfo()}</p>
+                    <p>
+                        {convertNameToInfo()}
+                    </p>
                 </div>
             </div>
         </>
