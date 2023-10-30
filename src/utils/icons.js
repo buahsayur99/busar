49069174274799c2c184e13fb958d7c8cb59ae01
Muhammad Safaat <paatlupi01@gmail.com>
2023-10-30 07:58:56 +0700

    add icon SiNamecheap

--- src/utils/icons.js
+++ src/utils/icons.js
@@ -1,12 +1,16 @@
-import { FaUserAlt } from "react-icons/fa"; // Icon User
+import { FaUserAlt, FaUsers, FaHome } from "react-icons/fa"; // Icon User, all users, Home
 import { PiLockKeyFill } from "react-icons/pi" // Closed Lock
 import { PiLockKeyOpenFill } from "react-icons/pi" // Open Lock
-import { IoIosCloseCircle, IoIosArrowDown } from "react-icons/io" // Close X
-import { RiLockPasswordFill } from "react-icons/ri" // Lock FormForgetPassword
-import { BsCheck2All, BsTrashFill, BsCheckAll } from "react-icons/bs" // Check list 2, Tempat sampah, Check List 2 tebal
-import { MdDeselect, MdSelectAll } from "react-icons/md" // disabled select, select all
-import { ImCheckboxChecked } from "react-icons/im" // CheckBox
-import { RxDashboard } from "react-icons/rx" // RxDashboard
+import { IoIosCloseCircle, IoIosArrowDown } from "react-icons/io" // Close X, Arrow Down
+import { RiLockPasswordFill, RiArrowRightDoubleLine, RiArrowLeftDoubleLine } from "react-icons/ri" // Lock FormForgetPassword, Arrow 2 right, Arrow 2 left
+import { BsCheck2All, BsTrashFill, BsCheckAll } from "react-icons/bs" // Check list 2, Tempat sampah, Check List 2 tebal,
+import { MdDeselect, MdSelectAll, MdProductionQuantityLimits, MdOutlineModeEditOutline } from "react-icons/md" // disabled select, select all, product,
+import { ImCheckboxChecked, ImBin } from "react-icons/im" // CheckBox, tempat sampah,
+import { RxDashboard } from "react-icons/rx" // RxDashboard,
+import { FiLogOut } from "react-icons/fi" // logout,
+import { GrEdit } from "react-icons/gr" // pensil edit,
+import { SiNamecheap } from "react-icons/si" // Name,
 
 
-export { FaUserAlt, PiLockKeyFill, PiLockKeyOpenFill, IoIosCloseCircle, RiLockPasswordFill, BsCheck2All, MdDeselect, MdSelectAll, ImCheckboxChecked, BsTrashFill, IoIosArrowDown, BsCheckAll, RxDashboard };
+
+export { FaUserAlt, FaUsers, PiLockKeyFill, PiLockKeyOpenFill, IoIosCloseCircle, RiLockPasswordFill, BsCheck2All, MdDeselect, MdSelectAll, ImCheckboxChecked, BsTrashFill, IoIosArrowDown, BsCheckAll, RxDashboard, MdProductionQuantityLimits, RiArrowRightDoubleLine, RiArrowLeftDoubleLine, FaHome, FiLogOut, ImBin, GrEdit, MdOutlineModeEditOutline };
