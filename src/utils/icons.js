3858a51a1c8edd54deb124e10de07550b1f93111
Muhammad Safaat <paatlupi01@gmail.com>
2023-10-31 01:51:19 +0700

    add icon amount and price

--- src/utils/icons.js
+++ src/utils/icons.js
@@ -1,10 +1,10 @@
-import { FaUserAlt, FaUsers, FaHome } from "react-icons/fa"; // Icon User, all users, Home
+import { FaUserAlt, FaUsers, FaHome, FaSortAmountUp } from "react-icons/fa"; // Icon User, all users, Home,Jumlah
 import { PiLockKeyFill } from "react-icons/pi" // Closed Lock
 import { PiLockKeyOpenFill } from "react-icons/pi" // Open Lock
 import { IoIosCloseCircle, IoIosArrowDown } from "react-icons/io" // Close X, Arrow Down
 import { RiLockPasswordFill, RiArrowRightDoubleLine, RiArrowLeftDoubleLine } from "react-icons/ri" // Lock FormForgetPassword, Arrow 2 right, Arrow 2 left
 import { BsCheck2All, BsTrashFill, BsCheckAll } from "react-icons/bs" // Check list 2, Tempat sampah, Check List 2 tebal,
-import { MdDeselect, MdSelectAll, MdProductionQuantityLimits, MdOutlineModeEditOutline } from "react-icons/md" // disabled select, select all, product,
+import { MdDeselect, MdSelectAll, MdProductionQuantityLimits, MdOutlineModeEditOutline, MdOutlinePriceChange } from "react-icons/md" // disabled select, select all, product, Harga,
 import { ImCheckboxChecked, ImBin } from "react-icons/im" // CheckBox, tempat sampah,
 import { RxDashboard } from "react-icons/rx" // RxDashboard,
 import { FiLogOut } from "react-icons/fi" // logout,
@@ -13,4 +13,4 @@ import { SiNamecheap } from "react-icons/si" // Name,
 
 
 
-export { FaUserAlt, FaUsers, PiLockKeyFill, PiLockKeyOpenFill, IoIosCloseCircle, RiLockPasswordFill, BsCheck2All, MdDeselect, MdSelectAll, ImCheckboxChecked, BsTrashFill, IoIosArrowDown, BsCheckAll, RxDashboard, MdProductionQuantityLimits, RiArrowRightDoubleLine, RiArrowLeftDoubleLine, FaHome, FiLogOut, ImBin, GrEdit, MdOutlineModeEditOutline, SiNamecheap };
+export { FaUserAlt, FaUsers, PiLockKeyFill, PiLockKeyOpenFill, IoIosCloseCircle, RiLockPasswordFill, BsCheck2All, MdDeselect, MdSelectAll, ImCheckboxChecked, BsTrashFill, IoIosArrowDown, BsCheckAll, RxDashboard, MdProductionQuantityLimits, RiArrowRightDoubleLine, RiArrowLeftDoubleLine, FaHome, FiLogOut, ImBin, GrEdit, MdOutlineModeEditOutline, SiNamecheap, FaSortAmountUp, MdOutlinePriceChange };
