2db80ba1b7493597bc434c01e0cbcf2b266b133b
Muhammad Safaat <paatlupi01@gmail.com>
2023-11-02 03:19:44 +0700

    add icon UploadFile and Image

--- src/utils/icons.js
+++ src/utils/icons.js
@@ -3,14 +3,15 @@ import { PiLockKeyFill } from "react-icons/pi" // Closed Lock
 import { PiLockKeyOpenFill } from "react-icons/pi" // Open Lock
 import { IoIosCloseCircle, IoIosArrowDown } from "react-icons/io" // Close X, Arrow Down
 import { RiLockPasswordFill, RiArrowRightDoubleLine, RiArrowLeftDoubleLine } from "react-icons/ri" // Lock FormForgetPassword, Arrow 2 right, Arrow 2 left
-import { BsCheck2All, BsTrashFill, BsCheckAll } from "react-icons/bs" // Check list 2, Tempat sampah, Check List 2 tebal,
+import { BsCheck2All, BsTrashFill, BsCheckAll, BsFillImageFill } from "react-icons/bs" // Check list 2, Tempat sampah, Check List 2 tebal, Image
 import { MdDeselect, MdSelectAll, MdProductionQuantityLimits, MdOutlineModeEditOutline, MdOutlinePriceChange } from "react-icons/md" // disabled select, select all, product, Harga,
 import { ImCheckboxChecked, ImBin } from "react-icons/im" // CheckBox, tempat sampah,
 import { RxDashboard } from "react-icons/rx" // RxDashboard,
 import { FiLogOut } from "react-icons/fi" // logout,
 import { GrEdit } from "react-icons/gr" // pensil edit,
 import { SiNamecheap } from "react-icons/si" // Name,
+import { BiSolidCloudUpload } from "react-icons/bi" // Upload File,
 
 
 
-export { FaUserAlt, FaUsers, PiLockKeyFill, PiLockKeyOpenFill, IoIosCloseCircle, RiLockPasswordFill, BsCheck2All, MdDeselect, MdSelectAll, ImCheckboxChecked, BsTrashFill, IoIosArrowDown, BsCheckAll, RxDashboard, MdProductionQuantityLimits, RiArrowRightDoubleLine, RiArrowLeftDoubleLine, FaHome, FiLogOut, ImBin, GrEdit, MdOutlineModeEditOutline, SiNamecheap, FaSortAmountUp, MdOutlinePriceChange };
+export { FaUserAlt, FaUsers, PiLockKeyFill, PiLockKeyOpenFill, IoIosCloseCircle, RiLockPasswordFill, BsCheck2All, MdDeselect, MdSelectAll, ImCheckboxChecked, BsTrashFill, IoIosArrowDown, BsCheckAll, RxDashboard, MdProductionQuantityLimits, RiArrowRightDoubleLine, RiArrowLeftDoubleLine, FaHome, FiLogOut, ImBin, GrEdit, MdOutlineModeEditOutline, SiNamecheap, FaSortAmountUp, MdOutlinePriceChange, BiSolidCloudUpload, BsFillImageFill };
