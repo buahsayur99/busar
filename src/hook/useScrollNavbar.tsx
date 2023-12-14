0e22f5544a4792a3c443e37bc15f5316058f06d5
Muhammad Safaat <paatlupi01@gmail.com>
2023-12-14 16:54:17 +0700

    added state scrolls

--- src/hook/useScrollNavbar.tsx
+++ src/hook/useScrollNavbar.tsx
@@ -4,16 +4,24 @@ type UseScrollProps = boolean
 
 export const useScrollNavbar = () => {
     const [scrolled, setScrolled] = useState<UseScrollProps>(false);
+    const [scrolls, setScrolls] = useState<number>(0);
 
     useEffect(() => {
         const onScroll = () => {
-            if (window.scrollY > 80) return setScrolled(true)
-            if (window.scrollY === 0) return setScrolled(false)
+            if (window.scrollY > 80) {
+                setScrolls(0)
+                return setScrolled(true)
+            }
+            if (window.scrollY === 0) {
+                setScrolls(0)
+                return setScrolled(false)
+            }
+            return setScrolls(-window.scrollY)
         }
 
         window.addEventListener("scroll", onScroll);
         return () => window.removeEventListener("scroll", onScroll);
     }, [])
 
-    return { scrolled };
+    return { scrolled, scrolls };
 }
