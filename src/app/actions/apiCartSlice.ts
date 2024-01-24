2862acaec6a3090c357ea52c851c5c670665cc4a
Muhammad Safaat <paatlupi01@gmail.com>
2024-01-24 13:37:30 +0700

    create function updateCart and deleteCart, create reducers activeCarts, handleCheckedCart, updateCheckedCart, and handleResetData

--- src/app/actions/apiCartSlice.ts
+++ src/app/actions/apiCartSlice.ts
@@ -1,19 +1,36 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { productProps } from "./apiProductSlice";
 
 type apiParameterProps = {
-    data: {
-        idProduct?: number,
-        amount: number,
-        urlImage: string,
-        price: number,
-    },
+    data: productProps | DataCartProps;
     link: string
 }
 
-const initialState = {
+export type DataCartProps = {
+    id: number,
+    idProduct: number,
+    nameProduct: string,
+    amount: number,
+    price: number,
+    totalPrice: number,
+    urlImage: string,
+    uuidUser: string
+}
+
+type InitialStateProps = {
+    dataCart: DataCartProps[],
+    isLoadingCart: boolean,
+    isMessageCart: string,
+    activeCart: boolean,
+    checkedCart: DataCartProps[]
+}
+
+const initialState: InitialStateProps = {
     dataCart: [],
-    isLoadingCart: false,
+    isLoadingCart: true,
     isMessageCart: "",
+    activeCart: false,
+    checkedCart: []
 }
 
 export const getCart = createAsyncThunk("api/getCart", async (link: string, { rejectWithValue }) => {
@@ -47,12 +64,72 @@ export const addCart = createAsyncThunk("api/addCart", async ({ data, link }: ap
     }
 });
 
+export const updateCart = createAsyncThunk("api/updateCart", async ({ data, link }: apiParameterProps, { rejectWithValue }) => {
+    try {
+        const response = await fetch(link, {
+            method: "PATCH",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ ...data })
+        });
+        const responseData = response.json();
+
+        if (response.ok) return responseData;
+        return rejectWithValue(responseData);
+    } catch (error: any) {
+        throw new Error(error.message);
+    }
+})
+
+export const deleteCart = createAsyncThunk("api/deleteCart", async ({ data, link }: { data: { arrayId: number[] }, link: string }, { rejectWithValue }) => {
+    const response = await fetch(link, {
+        method: "DELETE",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ ...data })
+    })
+    const responseData = await response.json();
+
+    if (response.ok) return responseData
+    return rejectWithValue(responseData)
+})
+
 const apiCartSlice = createSlice({
     name: 'api cart',
     initialState,
     reducers: {
         resetIsMessageCart: (state) => {
             state.isMessageCart = ""
+        },
+        activeCarts: (state, action) => {
+            state.activeCart = action.payload
+        },
+        handleCheckedCart: (state, action) => {
+            if (action.payload === "selectAll") {
+                if (state.checkedCart.length !== state.dataCart.length) {
+                    state.checkedCart = state.dataCart
+                } else {
+                    state.checkedCart = []
+                }
+            } else {
+                const dataChecked = state.checkedCart.filter((data) => {
+                    return data.id === action.payload.id
+                });
+
+                if (dataChecked.length !== 0) {
+                    const NewDataChecked = state.checkedCart.filter((data) => {
+                        return data.id !== action.payload.id
+                    });
+
+                    state.checkedCart = NewDataChecked
+                } else {
+                    state.checkedCart = [...state.checkedCart, action.payload]
+                }
+            }
+        },
+        updateCheckedCart: (state, action) => {
+            state.checkedCart = action.payload
+        },
+        handleResetData: (state) => {
+            state.dataCart = []
         }
     },
     extraReducers: (builder) => {
@@ -79,8 +156,32 @@ const apiCartSlice = createSlice({
                 state.isLoadingCart = false
                 state.isMessageCart = action.payload.message
             })
+
+            .addCase(updateCart.pending, (state) => {
+                state.isLoadingCart = true
+            })
+            .addCase(updateCart.fulfilled, (state, action) => {
+                state.isLoadingCart = false
+                state.isMessageCart = action.payload.message
+            })
+            .addCase(updateCart.rejected, (state, action: any) => {
+                state.isLoadingCart = false
+                state.isMessageCart = action.payload.message
+            })
+
+            .addCase(deleteCart.pending, (state) => {
+                state.isLoadingCart = true
+            })
+            .addCase(deleteCart.fulfilled, (state, action) => {
+                state.isLoadingCart = false
+                state.isMessageCart = action.payload.message
+            })
+            .addCase(deleteCart.rejected, (state, action: any) => {
+                state.isLoadingCart = false
+                state.isMessageCart = action.payload.message
+            })
     }
 })
 
-export const { resetIsMessageCart } = apiCartSlice.actions;
+export const { resetIsMessageCart, activeCarts, handleCheckedCart, updateCheckedCart, handleResetData } = apiCartSlice.actions;
 export default apiCartSlice.reducer;
