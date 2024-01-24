25508efff596c1bc448a5c64295e4e63e0034b40
Muhammad Safaat <paatlupi01@gmail.com>
2024-01-24 15:46:45 +0700

    remove useAppSelector apiProduct,  remove useEffect handleGetProduct

--- src/hook/useGetProduct.tsx
+++ src/hook/useGetProduct.tsx
@@ -1,20 +1,14 @@
-import { useCallback, useEffect } from "react";
-import { useAppDispatch, useAppSelector } from "../app/hooks";
+import { useCallback } from "react";
+import { useAppDispatch } from "../app/hooks";
 import { getProduct } from "../app/actions/apiProductSlice";
 
 export const useGetProduct = () => {
     const dispatch = useAppDispatch();
-    const { dataProductApi } = useAppSelector(state => state.apiProduct);
 
     const handleGetProduct = useCallback(() => {
         const link = `${process.env.REACT_APP_API_URL_LOCAL}/products`;
         dispatch(getProduct(link))
     }, [dispatch]);
-
-
-    useEffect(() => {
-        if (dataProductApi.length === 0) return handleGetProduct();
-    }, [handleGetProduct, dataProductApi.length])
 
     return { handleGetProduct }
 }
