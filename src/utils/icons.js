a13b54ceab725fc664683bbe0dbb4cee3a82845a
Muhammad Safaat <paatlupi01@gmail.com>
2024-01-24 14:01:07 +0700

    add icon IoIosArrowUp, and  MdLocationPin

--- src/utils/icons.js
+++ src/utils/icons.js
@@ -1,10 +1,10 @@
 import { FaUserAlt, FaUsers, FaHome, FaSortAmountUp, FaFacebookF, FaTwitter, FaLinkedinIn } from "react-icons/fa"; // Icon User, all users, Home, Jumlah, Facebook, Twitter, Linkedin
 import { PiLockKeyFill } from "react-icons/pi" // Closed Lock
 import { PiLockKeyOpenFill } from "react-icons/pi" // Open Lock
-import { IoIosCloseCircle, IoIosArrowDown, IoLogoYoutube } from "react-icons/io" // Close X dengan bg, Arrow Down, Youtube
+import { IoIosCloseCircle, IoIosArrowDown, IoIosArrowUp, IoLogoYoutube } from "react-icons/io" // Close X dengan bg, Arrow Down Line, Arrow Up Line, Youtube
 import { RiLockPasswordFill, RiArrowRightDoubleLine, RiArrowLeftDoubleLine } from "react-icons/ri" // Lock FormForgetPassword, Arrow 2 right, Arrow 2 left
 import { BsCheck2All, BsTrashFill, BsCheckAll, BsFillImageFill, BsPlusCircle, BsArrowLeft, BsArrowRight, BsSearch } from "react-icons/bs" // Check list 2, Tempat sampah, Check List 2 tebal, Image, incon +, Arrow left, Arrow Right, Search
-import { MdDeselect, MdSelectAll, MdProductionQuantityLimits, MdOutlineModeEditOutline, MdOutlinePriceChange, MdOutlineKeyboardArrowRight, MdOutlineKeyboardArrowLeft } from "react-icons/md" // disabled select, select all, product, Harga, arrow right line, arrow left line
+import { MdDeselect, MdSelectAll, MdProductionQuantityLimits, MdOutlineModeEditOutline, MdOutlinePriceChange, MdOutlineKeyboardArrowRight, MdOutlineKeyboardArrowLeft, MdLocationPin } from "react-icons/md" // disabled select, select all, product, Harga, arrow right line, arrow left line, Location
 import { ImCheckboxChecked, ImBin } from "react-icons/im" // CheckBox, tempat sampah,
 import { RxDashboard } from "react-icons/rx" // RxDashboard,
 import { FiLogOut } from "react-icons/fi" // logout,
@@ -17,5 +17,5 @@ import { TiSocialInstagram } from "react-icons/ti"; // Instagram
 import { IoCloseSharp } from "react-icons/io5"; // Close X tanpa bg
 
 export {
-    FaUserAlt, FaUsers, PiLockKeyFill, PiLockKeyOpenFill, IoIosCloseCircle, RiLockPasswordFill, BsCheck2All, MdDeselect, MdSelectAll, ImCheckboxChecked, BsTrashFill, IoIosArrowDown, BsCheckAll, RxDashboard, MdProductionQuantityLimits, RiArrowRightDoubleLine, RiArrowLeftDoubleLine, FaHome, FiLogOut, ImBin, GrEdit, MdOutlineModeEditOutline, SiNamecheap, FaSortAmountUp, MdOutlinePriceChange, BiSolidCloudUpload, BsFillImageFill, BsPlusCircle, BsArrowLeft, BsArrowRight, GoHeart, GoHeartFill, SlBasket, MdOutlineKeyboardArrowRight, MdOutlineKeyboardArrowLeft, FaFacebookF, FaTwitter, IoLogoYoutube, TiSocialInstagram, FaLinkedinIn, BsSearch, IoCloseSharp
+    FaUserAlt, FaUsers, PiLockKeyFill, PiLockKeyOpenFill, IoIosCloseCircle, RiLockPasswordFill, BsCheck2All, MdDeselect, MdSelectAll, ImCheckboxChecked, BsTrashFill, IoIosArrowDown, BsCheckAll, RxDashboard, MdProductionQuantityLimits, RiArrowRightDoubleLine, RiArrowLeftDoubleLine, FaHome, FiLogOut, ImBin, GrEdit, MdOutlineModeEditOutline, SiNamecheap, FaSortAmountUp, MdOutlinePriceChange, BiSolidCloudUpload, BsFillImageFill, BsPlusCircle, BsArrowLeft, BsArrowRight, GoHeart, GoHeartFill, SlBasket, MdOutlineKeyboardArrowRight, MdOutlineKeyboardArrowLeft, FaFacebookF, FaTwitter, IoLogoYoutube, TiSocialInstagram, FaLinkedinIn, BsSearch, IoCloseSharp, IoIosArrowUp, MdLocationPin
 };
