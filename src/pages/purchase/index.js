86b1cc890ee30a8ffdb0c3308cd67630df3e444d
Muhammad Safaat <paatlupi01@gmail.com>
2024-05-12 02:04:56 +0700

    create barrel pages purchase

---
+++ src/pages/purchase/index.js
@@ -0,0 +1,8 @@
+import { AllPurchase } from "./AllPurchase";
+import { CancelledPurchase } from "./CancelledPurchase";
+import { PackagedPurchase } from "./PackagedPurchase";
+import { PendingPurchase } from "./PendingPurchase";
+import { SendPurchase } from "./SendPurchase";
+import { SuccessPurchase } from "./SuccessPurchase";
+
+export { AllPurchase, CancelledPurchase, PackagedPurchase, PendingPurchase, SendPurchase, SuccessPurchase }
