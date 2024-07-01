eb66b8b08db846cfeaf8c40baf31a6e85f3996c6
Muhammad Safaat <paatlupi01@gmail.com>
2024-07-01 22:37:38 +0700

    create index import feature Navbar

---
+++ src/features/navbar/index.js
@@ -0,0 +1,8 @@
+import { AutoComplete } from "./AutoComplete";
+import { IconBasket } from "./IconBasket";
+import { MobileUsers } from "./MobileUsers";
+import { NavigationBar } from "./NavigationBar";
+import { SearchProduct } from "./SearchProduct";
+import { UsersLogin } from "./UsersLogin";
+
+export { AutoComplete, IconBasket, MobileUsers, NavigationBar, SearchProduct, UsersLogin }
